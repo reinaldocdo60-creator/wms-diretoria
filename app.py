5e6b4677aa72288d4f3b49480ef3db258243f720
reinaldocdo60-creator <reinaldocdo60@gmail.com>
2026-09-06 19:31:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -143,7 +143,7 @@ if not st.session_state["autenticado"]:
 # =========================================================
 # BARRA LATERAL (MENU E PERFIL)
 # =========================================================
-st.sidebar.title("📦 WMS Nuvem")
+st.sidebar.title("📦 WMS Pnet")
 st.sidebar.write(f"👤 **Usuário:** {st.session_state['usuario_logado']}")
 st.sidebar.write(f"🛡️ **Perfil:** `{st.session_state['perfil']}`")
 
